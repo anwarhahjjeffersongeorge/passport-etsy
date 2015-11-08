7f0dbf002c1dc4ef7cebb6d42b6e7252a73ce843
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2015-11-08 10:57:43 -0500

    Scaffolding
    1. Yeoman scaffolding with generator-node:
    https://github.com/yeoman/generator-node
    2. Dependencies:
    passport-oauth: https://github.com/jaredhanson/passport-oauth
    3. Dev Dependencies:
    chai: chaijs.com
    chai-passport-strategy:
    https://github.com/jaredhanson/chai-passport-strategy

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import passportEtsy from '../lib';
+
+describe('passport-etsy', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
