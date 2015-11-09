15cea17c5c327520e491f9a20b4a58d7e39c501f
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2015-11-09 12:16:11 -0500

    make strategy, index.js
    Update es-lint rules, create strategy, export via index
    
    Also add comments to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,14 @@ require('babel-core/register');
 gulp.task('static', function () {
   return gulp.src('**/*.js')
     .pipe(excludeGitignore())
+    // eslint() attaches the lint output to the eslint property
+    // of the file object so it can be used by other modules.
     .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
 });
 
